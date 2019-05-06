847294e692f56dc0bfba5e29bef51ad4869fe4bc
armayndo <armayndo1@yahoo.com>
2019-05-06 08:24:05 +0700

    update after review

--- src/main/java/com/mitrais/blog/unittest/controller/BookController.java
+++ src/main/java/com/mitrais/blog/unittest/controller/BookController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.mitrais.blog.unittest.domain.Book;
 import com.mitrais.blog.unittest.domain.Item;
@@ -48,6 +51,24 @@ public class BookController {
 		//ResponseEntity<List<Book>> responseEntity = new ResponseEntity<>(rstBook, HttpStatus.OK);
 		
 		return ResponseEntity.ok(rstBook);
+	}
+	
+	@PostMapping("/regbook")
+	public ResponseEntity<?> regBook(UriComponentsBuilder b ,
+			@RequestParam(value="title",required=true) String title ,
+			@RequestParam(value="isbn",required=true) String isbn ,
+			@RequestParam(value="aut",required=true) String aut) {
+		Book newBook = new Book();
+		newBook.setAuthor(aut);
+		newBook.setIsbn(isbn);
+		newBook.setTitle(title);
+		newBook.setShelf(null);
+		newBook = bookService.addBook(newBook);
+		
+		//UriComponents uriComponents = b.path("/findbyid/{id}").buildAndExpand(newBook.getId());
+		UriComponents uriComponents = b.path("/showall").build();
+
+		return ResponseEntity.created(uriComponents.toUri()).build();
 	}
 	
 	
